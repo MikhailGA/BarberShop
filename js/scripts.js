9f740153a3beffb727d6db8a82dab6112608a3f9
MikhailGA <32871511+MikhailGA@users.noreply.github.com>
2017-10-28 13:24:49 +0300

    Добавляем JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,41 @@
 var link = document.querySelector(".login");
+var map_link = document.querySelector(".modal_map");
+
 var popup = document.querySelector(".modal-content");
+var popup_map = document.querySelector(".modal-content-map");
 
-
+var btn_close = document.querySelectorAll(".modal-content-close");
 
 link.addEventListener("click",function (event) {
-    event.preventDefault();
-    popup.classList.add("show-modal-content");
+    modal_content_show(event, popup);
 });
+
+map_link.addEventListener("click",function (event) {
+    modal_content_show(event, popup_map);
+});
+
+var i = 0;
+while (i<btn_close.length){
+    btn_close[i].addEventListener("click",function (event){
+        modal_content_hide(event);
+    })
+    i++;
+}
+
+function modal_content_show(event, selector) {
+    event.preventDefault();
+    selector.classList.remove("hide-modal-content");
+    selector.classList.add("show-modal-content");
+}
+
+function modal_content_hide(event) {
+    event.preventDefault();
+    if (popup.classList.contains("show-modal-content")) {
+        popup.classList.remove("show-modal-content");
+        popup.classList.add("hide-modal-content");
+    }
+    if (popup_map.classList.contains("show-modal-content")) {
+            popup_map.classList.remove("show-modal-content");
+            popup_map.classList.add("hide-modal-content");
+    }
+}
